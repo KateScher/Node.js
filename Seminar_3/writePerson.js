88eff5689b502c35b96c7418986633f48d7c8c8d
Ekaterina Scherbakova <kat-m1@mail.ru>
2024-05-15 20:09:23 +0500

    Add seminar 3

--- Seminar_3/writePerson.js
+++ Seminar_3/writePerson.js
@@ -13,3 +13,19 @@
 - Для определения пути к файлу, используйте модуль path и его метод .join(),
 а также глобальное свойство __dirname
 - Используйте синхронный метод записи в файл */
+
+const fs = require("fs");
+const path = require("path");
+
+const person = {
+  name: "Ivan",
+  surname: "Ivanov",
+  age: 30,
+  city: "Moscow",
+};
+
+// модуль fs для работы с файлами
+fs.writeFileSync(
+  path.join(__dirname, "person.json"),
+  JSON.stringify(person, null, 2)
+); // название создаваемого файла - person.json, null - чтобы ничего 'не прилетело в файл' и 2 - кол-во пробелов
